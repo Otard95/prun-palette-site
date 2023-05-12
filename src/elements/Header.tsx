ba1a2bde7c9c6cf20585e6035e09fb441d3a0cdd
Stian Myklebostad (Work) <stian.myklebostad@schibsted.com>
2023-05-12 16:21:16 +0200

    feat: Improve responsiveness

--- src/elements/Header.tsx
+++ src/elements/Header.tsx
@@ -1,7 +1,8 @@
-import { Component, createMemo } from 'solid-js'
+import { Component, createMemo, createSignal } from 'solid-js'
 import { A, useLocation } from '@solidjs/router'
 
 import styles from './Header.module.css'
+import burger from '../assets/burger-light.png'
 
 import Logo from '../components/Logo'
 import ExtLink from '../components/ExtLink'
@@ -16,14 +17,20 @@ const links: { name: string, href: string }[] = [
 const Header: Component = () => {
   const location = useLocation()
   const pathname = createMemo(() => location.pathname)
+  const [ open, setOpen ] = createSignal(false)
+
   return (
     <header class={styles.header}>
       <span style={{'grid-area': 'logo'}}>
         <Logo />
       </span>
       <div class={styles['ext']}>
-        <ExtensionGet />
-        <ExtLink href='https://www.google.com/chrome/'>Github</ExtLink>
+        <div class={open() ? styles['ext-burger-show'] : ''}>
+          <button onClick={() => setOpen(!open())} class={styles['ext-burger']} style={`background-image: url("${burger}");`}/>
+          <p>Get the extension or check out the source code!</p>
+          <ExtensionGet />
+          <ExtLink href='https://www.google.com/chrome/'>Github</ExtLink>
+        </div>
       </div>
       <hr/>
       <nav>

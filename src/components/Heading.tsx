79d71ab23258b0281462d5e6e5d5b3f64f21521e
Stian Myklebostad (Work) <stian.myklebostad@schibsted.com>
2023-05-15 07:22:13 +0200

    feat: Ajust heading

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -2,35 +2,35 @@ import type { Component, PropsWithChildren } from 'solid-js'
 
 import styles from './Heading.module.css'
 
-interface HeaderProps {
+interface HeadingProps {
 }
 
-const h1: Component<PropsWithChildren<HeaderProps>> = (props) => (
+const h1: Component<PropsWithChildren<HeadingProps>> = (props) => (
   <h1 class={`${styles.heading} ${styles.h1}`}>
     {props.children}
   </h1>
 )
-const h2: Component<PropsWithChildren<HeaderProps>> = (props) => (
+const h2: Component<PropsWithChildren<HeadingProps>> = (props) => (
   <h2 class={`${styles.heading} ${styles.h2}`}>
     {props.children}
   </h2>
 )
-const h3: Component<PropsWithChildren<HeaderProps>> = (props) => (
+const h3: Component<PropsWithChildren<HeadingProps>> = (props) => (
   <h3 class={`${styles.heading} ${styles.h3}`}>
     {props.children}
   </h3>
 )
-const h4: Component<PropsWithChildren<HeaderProps>> = (props) => (
+const h4: Component<PropsWithChildren<HeadingProps>> = (props) => (
   <h4 class={`${styles.heading} ${styles.h4}`}>
     {props.children}
   </h4>
 )
-const h5: Component<PropsWithChildren<HeaderProps>> = (props) => (
+const h5: Component<PropsWithChildren<HeadingProps>> = (props) => (
   <h5 class={`${styles.heading} ${styles.h5}`}>
     {props.children}
   </h5>
 )
-const h6: Component<PropsWithChildren<HeaderProps>> = (props) => (
+const h6: Component<PropsWithChildren<HeadingProps>> = (props) => (
   <h6 class={`${styles.heading} ${styles.h6}`}>
     {props.children}
   </h6>
